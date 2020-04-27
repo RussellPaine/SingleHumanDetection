53687575aeccb2ffab267b07e48acf40e263c642
RussellPaine <russell.d.paine@gmail.com>
2020-04-27 16:17:04 +0100

    Added RoI classification

--- Desktop/FasterRCNN/main.py
+++ Desktop/FasterRCNN/main.py
@@ -13,7 +13,6 @@ labelsPath = os.path.join(os.getcwd(), "model", "classes.txt")
 LABELS = open(labelsPath).read().strip().split("\n")
 COLORS = np.random.randint(0, 255, size=(len(LABELS), 3), dtype="uint8")
 
-
 if  __name__ == "__main__":
 	print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
 
@@ -39,6 +38,7 @@ freq = cv2.getTickFrequency()
 camera = cv2.VideoCapture(0)
 ret = camera.set(3,1280)
 ret = camera.set(4,720)
+
 while(True):
 
 	t1 = cv2.getTickCount()
@@ -51,29 +51,37 @@ while(True):
 		[detection_boxes, detection_scores, detection_classes, num_detections],
 		feed_dict={image_tensor: frame_expanded})
 
-
 	boxes = np.squeeze(boxes)
 	classes = np.squeeze(classes).astype(np.int32)
 	scores = np.squeeze(scores)
 
 	height, width = frame.shape[:2]
 
+	RoIList = []
+
 	for i in range(boxes.shape[0]):
 		if classes[i] != 1:
 			continue
 		if scores[i] > 0.85:
-			
 			label = '{}: {}%'.format(LABELS[classes[i]], int(100*scores[i]))
 			y1, x1, y2, x2 = boxes[i] * np.array([height, width, height, width])
 			color= COLORS[classes[i]]
+
+			midX = x2 - x1
+			midY = y2 - y1
+
+			roi = frame[int(y1):int(y2), int(x1):int(x2)]
+			RoIList.append([midX / 2, midY / 2, midY, midX])
 			color = [int(c) for c in color]
+
 
 			cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), color, 2)
 			cv2.putText(frame, label, (int(x1), int(y1) - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
 
-	
 	cv2.putText(frame, "{0:.2f}".format(frame_rate_calc), (30,50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,0), 1, cv2.LINE_AA)
+	cv2.putText(frame, "{0}".format(len(RoIList)), (30,100), cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,0), 2, cv2.LINE_AA)
 	cv2.imshow('frame', frame)
+
 
 	t2 = cv2.getTickCount()
 	time1 = (t2-t1)/freq
